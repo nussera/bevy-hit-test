b90461963ef86ede866842f240c1b2fb2eb2aae9
Andreas Nusser <andreas.nusser@gmail.com>
2022-02-03 22:11:03 +0100

    Working hit test for svg-path

--- src/main.rs
+++ src/main.rs
@@ -10,52 +10,68 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 // This prints out "hello world" once every second
-                .with_run_criteria(FixedTimestep::step(30.0 / 60.0))
+                .with_run_criteria(FixedTimestep::step(1.0))
                 .with_system(hit_test_system)
         )
         .run();
 }
 
 fn setup_system(mut commands: Commands) {
-    let svg_doc_size = Vec2::new(100.,100.);
-    let shape = &shapes::SvgPathShape{
-        svg_path_string:"M 0 50 a 50 50 0 1 1 0 1z M 5 50 a 45 45 0 1 1 0 1z".to_owned(),
-        svg_doc_size_in_px:svg_doc_size.to_owned()
-    };
-    // let mut path_builder = PathBuilder::new();
-    // path_builder.move_to(Vec2::new(0.0, 30.0));
-    // path_builder.arc(Vec2::new(10.0, 10.0), Vec2::new(20.0, 20.0), 0.0, 0.0);
-    // path_builder.close();
-    // let path = path_builder.build();
-
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    add_rectangle(&mut commands, 25f32);
+    
+    //Spawn some rectangles for reference.
+    add_rectangle(&mut commands,25f32);
     add_rectangle(&mut commands,50f32);
     add_rectangle(&mut commands,100f32);
     add_rectangle(&mut commands,200f32);
+
+    // We can create a doughnut via an SVG Path. This will generate hits.
     commands.spawn_bundle(GeometryBuilder::build_as(
-            shape,
-            DrawMode::Stroke(StrokeMode::new(Color::LIME_GREEN, 1.0)),
-            Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
-                ..Default::default()
-            },
-        ))
-        .insert(Name::new("test circle"));
-    // commands.spawn_bundle(ShapeBundle{
-    //     mode: DrawMode::Stroke(StrokeMode::new(Color::LIME_GREEN, 1.0)),
-    //     path: path,
-    //     ..Default::default()
-    // })
-    //     .insert(Name::new("test circle"));
+        &shapes::SvgPathShape{
+            // When used in a browser, we need to flip the sweep flag.
+            // M 0 50 a 50 50 0 1 >1< 0 1z M 5 50 a 45 45 0 1 >1< 0 1
+            // Maybe a bug in lyon?
+            svg_path_string:"M 0 50 a 50 50 0 1 0 0 1z M 5 50 a 45 45 0 1 0 0 1z".to_owned(),
+            svg_doc_size_in_px: Vec2::new(100.,100.).to_owned()
+        },
+        DrawMode::Fill(FillMode{
+            options: FillOptions::DEFAULT,
+            color: Color::LIME_GREEN
+        }),
+        Transform {
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            ..Default::default()
+        },
+    ))
+    .insert(Name::new("test circle"));
+
+    // Or via the lyon PathBuilder. Unfortunately, this does not generate any hits.
+    /*let mut path_builder = PathBuilder::new();
+    path_builder.move_to(Vec2::new(45., 0.0));
+    path_builder.arc(Vec2::new(0.0, 0.0), Vec2::new(45.0, 45.0), 6.2, 0.0);
+    path_builder.close();
+    path_builder.move_to(Vec2::new(50., 0.0));
+    path_builder.arc(Vec2::new(0.0, 0.0), Vec2::new(50.0, 50.0), 6.2, 0.0);
+    path_builder.close();
+    let path = path_builder.build();
+
+    commands.spawn_bundle(ShapeBundle{
+        mode: DrawMode::Fill(FillMode{
+            options: FillOptions::DEFAULT,
+            color: Color::LIME_GREEN
+        }),
+        path,
+        ..Default::default()
+    })
+    .insert(Name::new("test circle"));*/
 }
 
 fn hit_test_system(query: Query<&Path, With<Name>>) {
     let path = query.get_single().unwrap();
     let pathslices = path.0.as_slice();
-    // let itr = pathslices.into_iter();
     println!("***********");
-    for x in 0..100 {
+    //We expect hits at 45<=x<=50
+    for x in 0..55 {
         let point = point(x as f32, 0f32);
         let hit = hit_test::hit_test_path(&point, pathslices.iter(), FillRule::EvenOdd, 0.0001f32);
         println!("Hit at {x}: {hit}");
